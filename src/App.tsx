9ce8e095e1e478341957a3bce0d3016c4233828e
RemonHassan <fmremonhassan203@gmail.com>
2026-09-10 22:13:52 +0600

    Added all cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
+import { Suspense, useState } from "react";
 import "./App.css";
 import Banner from "./Components/Banner";
 import NavBaar from "./Components/NavBaar";
+import type { ITechnology } from "./Types/CardType";
+import AllCards from "./Components/AllCards";
 
+const cardsFetch = async (): Promise<ITechnology[]> => {
+  const res = await fetch("/public/data.json");
+  const data = await res.json();
+  return data;
+};
 function App() {
+  const [cardsPromise] = useState(() => cardsFetch());
   return (
     <>
       <NavBaar></NavBaar>
       <Banner></Banner>
+      <Suspense fallback={<div>Loading...</div>}>
+        <AllCards cardsPromise={cardsPromise}></AllCards>
+      </Suspense>
     </>
   );
 }
